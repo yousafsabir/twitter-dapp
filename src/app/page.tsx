76351f577e52cd060bd77622c4a1368fa2ed111c
yousafsabir <myousafdev@gmail.com>
2024-05-23 17:21:16 +0500

    chore: initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen font-mono flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">Twitter DApp</h1>
+    </main>
+  );
+}
